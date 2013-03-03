b91368139a7a97ea51a8444158ff0b7fa023b60b
Jibi Abraham <jibi.abrahm@gmail.com>
2013-03-03 17:16:27 +0530

    Antiscoll added in place of a marquee

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 $(function(){
+
+	var truncateLength = 250;
+	$(".truncate").each(function(){
+		var self = $(this), text = self.text();
+		if (text.length > truncateLength){
+			self.data("fullText", text);
+			self.text(text.slice(0, truncateLength) + "...");
+			self.append($("<a href='javascript:;' class='read_more'>read more</a>"));
+		}
+	});
+
+	$(".read_more").click(function(){
+		var self = $(this), text = self.parent().data("fullText");
+		self.parent().text(text);
+	});
+
 });
--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,40 @@
 $(function(){
 	jQuery('.camera_wrap').camera();
+
+	// Custom code for initialising the ticker plugin
+	$.get("/achievements.html", function(response){
+		var contents = $($.parseHTML(response)).find("#achievementsList"), 
+			antiscrollWrapper = $("<div class='antiscroll-wrap' style='height:75px;'><div class='antiscroll-inner' style='height:75px;'></div></div>");
+		contents.removeClass("with-fav-icons");
+
+		//  Setup the antiscroll
+		antiscrollWrapper.find("div").append(contents).end().appendTo("#ticker").antiscroll();
+
+		// Center the list elements
+		/*contents.find("li").each(function(){
+			var self = 
+		});*/
+
+		/*
+			Set a timer to keep scrolling the items
+			The scrollable item here is the antiscroll-inner element
+		*/
+		var timer, inner = antiscrollWrapper.find(".antiscroll-inner"), activeIndex = -1, newsFeedHeight = contents.height(),
+			newsFeed = contents.find("li"), newsLength = newsFeed.length, scrollTimer = 2000;
+		
+		timer = setInterval(function() {
+			++activeIndex;
+			activeIndex >= newsLength && (activeIndex = 0);
+			// Center the list element
+
+			/*newsFeed.eq(activeIndex).scrollintoview();*/
+
+		}, scrollTimer);
+
+	});
+
+	function centerElement (item) {
+		var parent = item.parent(), heightSelf = item.height();
+	}
+
 });
